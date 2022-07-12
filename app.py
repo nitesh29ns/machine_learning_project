6b7401c99124e1a8ea08dd2dca8f76216f9da956
nitesh29ns <niteshsharma29.ns@gmail.com>
2022-07-12 18:01:50 +0530

    changes ci/cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/", methods=('GET','POST'))
 def index():
-    return "this is my first ml project"
+    return "this is my first ml project --> before and after ci/cd pipline this is updated textgit  "
 
 if __name__=="__main__":
     app.run(debug=True)
